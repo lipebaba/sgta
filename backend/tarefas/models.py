0bdf1e19fe6803dd8b18434dbb7353918f890da6
lipebaba <felipebabateixeira@hotmail.com>
2026-03-23 21:11:22 -0300

    Atualizações

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        {'ABERTA','Aberta'},
+        {'EM ANDAMNETO', 'Em andamento'},
+        {'CONCLUIDA', 'Concluida'},
+        {'CANCELADA', 'Cancelada'}
+    ]
+
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    status = models.CharField(max_length=20, choices = STATUS_CHOICES, default='ABERTA')
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_entrega = models.DateField()
+
+    def __str__(self):
+        return self.titulo
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from .models import Tarefa
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list[any](tarefas), safe=False)
+
