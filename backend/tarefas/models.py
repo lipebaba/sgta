3659a8d2370a6ca62c6abe0ce79085075d235e28
lipebaba <felipebabateixeira@hotmail.com>
2026-03-24 20:33:48 -0300

    Atualizações no Models, Viwes e Urls

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
 class Tarefa(models.Model):
+
     STATUS_CHOICES = [
-        {'ABERTA','Aberta'},
-        {'EM ANDAMNETO', 'Em andamento'},
-        {'CONCLUIDA', 'Concluida'},
-        {'CANCELADA', 'Cancelada'}
+        ('ABERTA', 'Aberta'),
+        ('EM_ANDAMENTO', 'Em andamento'),
+        ('CONCLUIDA', 'Concluída'),
+        ('CANCELADA', 'Cancelada')
+    ]
+
+    PRIORIDADE_CHOICES = [
+        ('URGENTE', 'Urgente'),
+        ('NAO_URGENTE', 'Não urgente')
     ]
 
     titulo = models.CharField(max_length=255)
     descricao = models.TextField()
-    status = models.CharField(max_length=20, choices = STATUS_CHOICES, default='ABERTA')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='ABERTA')
+    prioridade = models.CharField(max_length=15, choices=PRIORIDADE_CHOICES, default='NAO_URGENTE')
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_entrega = models.DateField()
 
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import JsonResponse
 from .models import Tarefa
 
 def listar_tarefas(request):
     tarefas = Tarefa.objects.all().values()
-    return JsonResponse(list[any](tarefas), safe=False)
+    return JsonResponse(list(tarefas), safe=False)
 
+def listar_tarefas_abertas(request):
+    tarefas = Tarefa.objects.filter(status='ABERTA').values()
+    return JsonResponse(list(tarefas), safe=False)
